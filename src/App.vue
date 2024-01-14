e23d4bddf5ff80e6c95bd8a4e3cb2b8e43f29da9
dhia nabli <dhianabli@outlook.com>
2024-01-14 17:12:12 +0100

    started implementing graph functionality

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,65 @@
-<script setup>
-import {VNetworkGraph} from "v-network-graph";
+<script setup lang="ts">
+import { reactive, ref } from "vue"
+import {Nodes, Edges, VNetworkGraph} from "v-network-graph"
+import data from "./data.ts"
 
-const nodes = {
-    node1: { name: "Node 1" },
-    node2: { name: "Node 2" },
-    node3: { name: "Node 3" },
-    node4: { name: "Node 4" },
+const nodes: Nodes = reactive({ ...data.nodes })
+const edges: Edges = reactive({ ...data.edges })
+const nextNodeIndex = ref(Object.keys(nodes).length + 1)
+const nextEdgeIndex = ref(Object.keys(edges).length + 1)
+
+const selectedNodes = ref<string[]>([])
+const selectedEdges = ref<string[]>([])
+
+function addNode() {
+    const nodeId = `node${nextNodeIndex.value}`
+    const name = `N${nextNodeIndex.value}`
+    nodes[nodeId] = { name }
+    nextNodeIndex.value++
 }
 
-const edges = {
-    edge1: { source: "node1", target: "node2" },
-    edge2: { source: "node2", target: "node3" },
-    edge3: { source: "node3", target: "node4" },
+function removeNode() {
+    for (const nodeId of selectedNodes.value) {
+        delete nodes[nodeId]
+    }
+}
+
+function addEdge() {
+    if (selectedNodes.value.length !== 2) return
+    const [source, target] = selectedNodes.value
+    const edgeId = `edge${nextEdgeIndex.value}`
+    edges[edgeId] = { source, target }
+    nextEdgeIndex.value++
+}
+
+function removeEdge() {
+    for (const edgeId of selectedEdges.value) {
+        delete edges[edgeId]
+    }
 }
 </script>
 
 <template>
-    <v-network-graph
-        class="graph"
-        :nodes="nodes"
-        :edges="edges"
-    />
+    <div class="demo-control-panel bg-green-400">
+        <div>
+            <label>Node:</label>
+            <el-button @click="addNode">add</el-button>
+            <el-button :disabled="selectedNodes.length == 0" @click="removeNode">remove</el-button>
+        </div>
+        <div>
+            <label>Edge:</label>
+            <el-button :disabled="selectedNodes.length != 2" @click="addEdge">add</el-button>
+            <el-button :disabled="selectedEdges.length == 0" @click="removeEdge">remove</el-button>
+        </div>
+    </div>
+    <div style="height:100vh;width:100vw;">
+        <v-network-graph
+            v-model:selected-nodes="selectedNodes"
+            v-model:selected-edges="selectedEdges"
+            :nodes="nodes"
+            :edges="edges"
+            :layouts="data.layouts"
+            :configs="data.configs"
+        />
+    </div>
 </template>
-
-<style>
-.graph {
-    width: 800px;
-    height: 600px;
-    border: 1px solid #000;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import VNetworkGraph from "v-network-graph"
 import "v-network-graph/lib/style.css"
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
 createApp(App)
+    .use(ElementPlus)
     .use(VNetworkGraph)
     .mount('#app')
