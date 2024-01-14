6d481cac6be01087ed3a1b8432e7fd90fcdc0919
dhia nabli <dhianabli@outlook.com>
2024-01-14 15:59:31 +0100

    started implementing the nodes/graph

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
+import VNetworkGraph from "v-network-graph"
+import "v-network-graph/lib/style.css"
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(VNetworkGraph)
+    .mount('#app')
