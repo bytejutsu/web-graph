f2098a16c4b689b7c8250bb09ff77dce317d816d
Dhia Nabli <bytejutsuproject@gmail.com>
2024-05-08 15:21:32 +0100

    improved navbar and footer

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Web-Graph
 
 [https://bytejutsu.github.io/web-graph/](https://bytejutsu.github.io/web-graph/)
+
+`npm install`
+`npm run dev`
--- src/App.vue
+++ src/App.vue
@@ -40,18 +40,24 @@ function removeEdge() {
 </script>
 
 <template>
-    <div class="demo-control-panel bg-green-400">
-        <div>
+    <nav class="relative flex w-full flex-wrap items-center justify-between bg-blue-500 shadow-dark-mild mb-4">
+      <div class="flex w-full flex-wrap items-center justify-between">
+        <img src="../images/Flag_of_Tunisia.svg" alt="Flag of Tunisia" width="50" height="20"/>
+        <div class="flex">
+          <div>
             <label>Node:</label>
             <el-button @click="addNode">add</el-button>
             <el-button :disabled="selectedNodes.length == 0" @click="removeNode">remove</el-button>
-        </div>
-        <div>
+          </div>
+          <div>
             <label>Edge:</label>
             <el-button :disabled="selectedNodes.length != 2" @click="addEdge">add</el-button>
             <el-button :disabled="selectedEdges.length == 0" @click="removeEdge">remove</el-button>
+          </div>
         </div>
-    </div>
+        <span class="text-white mr-2 font-bold">Web Graph</span>
+      </div>
+    </nav>
     <div style="height:100vh;width:100vw;">
         <v-network-graph
             v-model:selected-nodes="selectedNodes"
@@ -62,4 +68,20 @@ function removeEdge() {
             :configs="data.configs"
         />
     </div>
+    <footer class="flex justify-between w-full fixed bottom-0 bg-black/5">
+      <!-- Author -->
+      <div class="p-6 text-left flex-1">
+        <span>Author:</span>
+        <span class="font-semibold">Dhia Nabli</span>
+      </div>
+      <!-- Copyright section -->
+      <div class="p-6 text-center flex-1">
+        <span>© 2024 Copyright:</span>
+        <a class="font-semibold" href="https://bytejutsu.com/">ByteJutsu</a>
+      </div>
+      <!-- Email -->
+      <div class="p-6 text-right flex-1">
+        <a class="font-semibold" href="mailto:bytejutsuproject@gmail.com">bytejutsuproject@gmail.com</a>
+      </div>
+    </footer>
 </template>
